d6a83277b47605d8b3b7f17d4a5c4c0cc6c48427
redlintles <treelands54@gmail.com>
2025-05-25 19:11:45 -0300

    parse method implemented to src/data/token_defs/tbs.rs

--- src/data/token_defs/tbs.rs
+++ src/data/token_defs/tbs.rs
@@ -25,4 +25,8 @@ impl TokenMethods for Tbs {
     fn get_string_repr() -> String {
         "tbs".to_string()
     }
+
+    fn parse(&self, input: &str) -> String {
+        String::from(input.trim())
+    }
 }
