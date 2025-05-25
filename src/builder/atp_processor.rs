35b21a5cd4167027a8b0004b8a90aa82ba7d1c3f
redlintles <treelands54@gmail.com>
2025-05-25 12:59:41 -0300

    First version of atp_processor implemented

--- src/builder/atp_processor.rs
+++ src/builder/atp_processor.rs
@@ -1,12 +1,44 @@
-use crate::data::AtpToken;
+use crate::{ data::AtpToken, parser::{ parse_token, writer::write_to_file } };
 
 pub struct AtpProcessor {
     pub(crate) tokens: Vec<AtpToken>,
 }
 
-trait AtpProcessorMethods {
-    fn write_to_file(path: &str) -> ();
-    fn process_string(string: &str) -> String;
+pub trait AtpProcessorMethods {
+    fn write_to_file(&self, path: &str) -> ();
+    fn process_string(&self, string: &str) -> String;
     // Step By Step
-    fn process_sbs_string(string: &str) -> String;
+    fn process_sbs_string(&self, string: &str) -> String;
+}
+
+impl AtpProcessorMethods for AtpProcessor {
+    fn write_to_file(&self, path: &str) -> () {
+        write_to_file(path, &self.tokens);
+    }
+
+    fn process_string(&self, string: &str) -> String {
+        let mut result = String::from(string);
+        for token in self.tokens.iter() {
+            result = parse_token(token.clone(), result.as_str());
+        }
+
+        result.to_string()
+    }
+
+    fn process_sbs_string(&self, string: &str) -> String {
+        let mut result = String::from(string);
+
+        let mut counter: i64 = 0;
+
+        for token in self.tokens.iter() {
+            let temp = parse_token(token.clone(), result.as_str());
+            println!("[{}] => [{}]: {} => {}", counter, counter + 1, result, temp);
+
+            result = String::from(temp);
+
+            counter += 1;
+        }
+
+        result.to_string()
+    }
 }
