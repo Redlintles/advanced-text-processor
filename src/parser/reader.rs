a11de1ee9ad67166a23820aea8718502ce9e3158
redlintles <treelands54@gmail.com>
2025-05-29 13:19:37 -0300

    Removido uso do enum AtpToken, isso possibilitou a substituição do match de tratamento à ele por algumas poucas linhas que tratam TokenMethods genericamente

--- src/parser/reader.rs
+++ src/parser/reader.rs
@@ -1,11 +1,11 @@
 use std::{ fs::OpenOptions, io::{ BufRead, BufReader } };
 
-use crate::data::token_defs::*;
+use crate::data::{ TokenMethods };
 
-use crate::data::{ AtpToken, TokenMethods };
+use super::get_supported_tokens;
 
-pub fn read_from_file(path: &str) -> Vec<AtpToken> {
-    let mut result: Vec<AtpToken> = Vec::new();
+pub fn read_from_file(path: &str) -> Vec<Box<dyn TokenMethods>> {
+    let mut result: Vec<Box<dyn TokenMethods>> = Vec::new();
 
     let file = OpenOptions::new()
         .read(true)
@@ -25,35 +25,12 @@ pub fn read_from_file(path: &str) -> Vec<AtpToken> {
 
         let parse_err_msg = "An ATP parsing error ocurred: Invalid Sintax";
 
-        let token: AtpToken = match &chunks {
-            x if x[0] == "atb" =>
-                AtpToken::Atb(atb::Atb::token_from_vec_params(chunks).expect(parse_err_msg)),
-            x if x[0] == "ate" =>
-                AtpToken::Ate(ate::Ate::token_from_vec_params(chunks).expect(parse_err_msg)),
-            x if x[0] == "raw" =>
-                AtpToken::Raw(raw::Raw::token_from_vec_params(chunks).expect(parse_err_msg)),
-            x if x[0] == "rfw" =>
-                AtpToken::Rfw(rfw::Rfw::token_from_vec_params(chunks).expect(parse_err_msg)),
-            x if x[0] == "dla" =>
-                AtpToken::Dla(dla::Dla::token_from_vec_params(chunks).expect(parse_err_msg)),
-            x if x[0] == "dlb" =>
-                AtpToken::Dlb(dlb::Dlb::token_from_vec_params(chunks).expect(parse_err_msg)),
-            x if x[0] == "dlc" =>
-                AtpToken::Dlc(dlc::Dlc::token_from_vec_params(chunks).expect(parse_err_msg)),
-            x if x[0] == "rtl" =>
-                AtpToken::Rtl(rtl::Rtl::token_from_vec_params(chunks).expect(parse_err_msg)),
-            x if x[0] == "rtr" =>
-                AtpToken::Rtr(rtr::Rtr::token_from_vec_params(chunks).expect(parse_err_msg)),
-            x if x[0] == "rpt" =>
-                AtpToken::Dlf(dlf::Dlf::token_from_vec_params(chunks).expect(parse_err_msg)),
-            x if x[0] == "tbs" => AtpToken::Tbs(tbs::Tbs::new()),
-            x if x[0] == "trs" => AtpToken::Trs(trs::Trs::new()),
-            x if x[0] == "tls" => AtpToken::Tls(tls::Tls::new()),
-            x if x[0] == "dll" => AtpToken::Dll(dll::Dll::new()),
-            x if x[0] == "dlf" => AtpToken::Dlf(dlf::Dlf::new()),
+        let supported_tokens = get_supported_tokens();
+        let token_factory = supported_tokens.get(&chunks[0]).expect(parse_err_msg);
 
-            _ => panic!("An ATP parsing error ocurred: Token not found"),
-        };
+        let mut token = token_factory();
+
+        token.token_from_vec_params(chunks);
 
         result.push(token);
     }
