7912b8458d1838e71d1dda391f7fdafcf4b8cf87
redlintles <treelands54@gmail.com>
2025-05-29 14:04:11 -0300

    Modificado AtpProcessor para trabalhar com Box<dyn TokenMethods> ao invés de AtpToken

--- src/builder/atp_processor.rs
+++ src/builder/atp_processor.rs
@@ -2,19 +2,19 @@ use std::collections::HashMap;
 
 use uuid::Uuid;
 
-use crate::data::AtpToken;
+use crate::data::{ TokenMethods };
 
 use crate::parser::parser::parse_token;
 use crate::parser::writer::write_to_file;
 
 pub struct AtpProcessor {
-    transforms: HashMap<String, Vec<AtpToken>>,
+    transforms: HashMap<String, Vec<Box<dyn TokenMethods>>>,
 }
 
 pub trait AtpProcessorMethods {
     fn write_to_file(&self, id: &str, path: &str) -> Result<(), String>;
 
-    fn add_transform(&mut self, tokens: &Vec<AtpToken>) -> String;
+    fn add_transform(&mut self, tokens: Vec<Box<dyn TokenMethods>>) -> String;
     fn process_string(&self, id: &str, string: &str) -> String;
     // Step By Step
     fn process_sbs_string(&self, id: &str, string: &str) -> String;
@@ -45,8 +45,8 @@ impl AtpProcessorMethods for AtpProcessor {
             .get(id)
             .expect("Token array not found, is id a valid transform identifier");
 
-        for token in tokens.into_iter() {
-            result = parse_token(token.clone(), result.as_str());
+        for token in tokens.iter() {
+            result = parse_token(token, result.as_str());
         }
 
         result.to_string()
@@ -61,8 +61,8 @@ impl AtpProcessorMethods for AtpProcessor {
             .get(id)
             .expect("Token array not found, is id a valid transform identifier");
 
-        for token in tokens.into_iter() {
-            let temp = parse_token(token.clone(), result.as_str());
+        for token in tokens.iter() {
+            let temp = parse_token(token, result.as_str());
             println!("[{}] => [{}]: {} => {}", counter, counter + 1, result, temp);
 
             result = String::from(temp);
@@ -73,7 +73,7 @@ impl AtpProcessorMethods for AtpProcessor {
         result.to_string()
     }
 
-    fn add_transform(&mut self, tokens: &Vec<AtpToken>) -> String {
+    fn add_transform(&mut self, tokens: Vec<Box<dyn TokenMethods>>) -> String {
         let identifier = Uuid::new_v4();
         self.transforms.insert(identifier.to_string(), tokens.clone());
 
