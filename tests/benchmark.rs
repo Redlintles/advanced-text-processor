0ce1e6884a7d0b33c5daefa8af4aed2bb454272b
redlintles <treelands54@gmail.com>
2025-05-29 17:09:09 -0300

    Adicionado teste process_all_tokens. simplificado teste com remoção de alguns prints

--- tests/benchmark.rs
+++ tests/benchmark.rs
@@ -35,14 +35,52 @@ pub mod benchmark {
 
             let processed_string = processor.process_sbs_string(&identifier, string_to_process);
 
+            println!("{}", processed_string);
             let elapsed = start.elapsed().as_secs_f64();
 
-            println!(
-                "A transformação de \"{}\" em \"{}\" levou: {:.6} Segundos",
-                string_to_process,
-                processed_string,
-                elapsed
-            );
+            total += elapsed;
+        }
+
+        let avg = total / (runs as f64);
+
+        println!("Média: {:.6} Segundos", avg);
+
+        assert!(avg < 0.003, "Executou muito devagar");
+    }
+    #[test]
+    fn process_all_tokens() {
+        let runs = 100;
+
+        let mut total = 0.0;
+
+        let (processor, identifier) = AtpBuilder::new()
+            .add_to_beginning("Banana")
+            .add_to_end("pizza")
+            .repeat(3)
+            .delete_after(20 as usize)
+            .delete_before(2 as usize)
+            .delete_chunk(0 as usize, 3 as usize)
+            .delete_first()
+            .delete_last()
+            .replace_all_with(r"a", "e")
+            .replace_first_with("L", "coxinha")
+            .rotate_left(1 as usize)
+            .rotate_right(2 as usize)
+            .trim_both()
+            .trim_left()
+            .trim_right()
+            .build();
+
+        for _ in 0..runs {
+            let start = Instant::now();
+
+            let string_to_process = "Banana Laranja cheia de canja";
+
+            let processed_string = processor.process_string(&identifier, string_to_process);
+
+            println!("{}", processed_string);
+
+            let elapsed = start.elapsed().as_secs_f64();
 
             total += elapsed;
         }
