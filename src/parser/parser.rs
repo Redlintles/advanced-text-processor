9bce3bcc565cf0137b701bacf22c745d675e8575
redlintles <treelands54@gmail.com>
2025-05-29 14:02:20 -0300

    Corrigido src/parser/parser.rs para receber Box<dyn TokenMethods>

--- src/parser/parser.rs
+++ src/parser/parser.rs
@@ -1,5 +1,5 @@
 use crate::data::{ TokenMethods };
 
-pub fn parse_token(token: impl TokenMethods, input: &str) -> String {
+pub fn parse_token(token: &Box<dyn TokenMethods>, input: &str) -> String {
     return token.parse(input);
 }
