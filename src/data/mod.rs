79193e2e591056384d0ca816e3d54ddee367e692
redlintles <treelands54@gmail.com>
2025-05-29 14:03:14 -0300

    Removido enum AtpToken do código

--- src/data/mod.rs
+++ src/data/mod.rs
@@ -1,40 +1,4 @@
 pub mod token_defs;
-
-use token_defs::ate::Ate;
-use token_defs::atb::Atb;
-use token_defs::rtr::Rtr;
-use token_defs::trs::Trs;
-use token_defs::tls::Tls;
-use token_defs::tbs::Tbs;
-use token_defs::raw::Raw;
-use token_defs::rfw::Rfw;
-use token_defs::dlf::Dlf;
-use token_defs::dll::Dll;
-use token_defs::dlb::Dlb;
-use token_defs::dla::Dla;
-use token_defs::dlc::Dlc;
-use token_defs::rtl::Rtl;
-use token_defs::rpt::Rpt;
-
-#[derive(Clone)]
-pub enum AtpToken {
-    Tbs(Tbs),
-    Tls(Tls),
-    Trs(Trs),
-    Raw(Raw),
-    Rfw(Rfw),
-    Ate(Ate),
-    Atb(Atb),
-    Dlb(Dlb),
-    Dla(Dla),
-    Dlf(Dlf),
-    Dll(Dll),
-    Dlc(Dlc),
-    Rtl(Rtl),
-    Rtr(Rtr),
-    Rpt(Rpt),
-}
-
 pub trait TokenMethodsClone {
     fn clone_box(&self) -> Box<dyn TokenMethods>;
 }
