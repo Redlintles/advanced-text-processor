cdc02e90c847248ffc19ca10f8efb724d7b1df67
redlintles <treelands54@gmail.com>
2025-05-29 13:18:16 -0300

    Removido uso do enum AtpToken em parser, alterado para impl TokenMethods

--- src/parser/parser.rs
+++ src/parser/parser.rs
@@ -1,21 +1,5 @@
-use crate::data::{ AtpToken, TokenMethods };
+use crate::data::{ TokenMethods };
 
-pub fn parse_token(token: AtpToken, input: &str) -> String {
-    match token {
-        AtpToken::Atb(x) => x.parse(input),
-        AtpToken::Tbs(x) => x.parse(input),
-        AtpToken::Tls(x) => x.parse(input),
-        AtpToken::Trs(x) => x.parse(input),
-        AtpToken::Raw(x) => x.parse(input),
-        AtpToken::Rfw(x) => x.parse(input),
-        AtpToken::Ate(x) => x.parse(input),
-        AtpToken::Dlb(x) => x.parse(input),
-        AtpToken::Dla(x) => x.parse(input),
-        AtpToken::Dlf(x) => x.parse(input),
-        AtpToken::Dll(x) => x.parse(input),
-        AtpToken::Dlc(x) => x.parse(input),
-        AtpToken::Rtl(x) => x.parse(input),
-        AtpToken::Rtr(x) => x.parse(input),
-        AtpToken::Rpt(x) => x.parse(input),
-    }
+pub fn parse_token(token: impl TokenMethods, input: &str) -> String {
+    return token.parse(input);
 }
