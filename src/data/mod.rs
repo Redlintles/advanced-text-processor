7bc17639d0b98f64c368d61985f364a919317ca3
redlintles <treelands54@gmail.com>
2025-05-29 14:01:49 -0300

    Implementada trait clone para Box<dyn TokenMethods>

--- src/data/mod.rs
+++ src/data/mod.rs
@@ -34,9 +34,26 @@ pub enum AtpToken {
     Rtr(Rtr),
     Rpt(Rpt),
 }
-pub trait TokenMethods {
+
+pub trait TokenMethodsClone {
+    fn clone_box(&self) -> Box<dyn TokenMethods>;
+}
+
+impl<T> TokenMethodsClone for T where T: 'static + TokenMethods + Clone {
+    fn clone_box(&self) -> Box<dyn TokenMethods> {
+        Box::new(self.clone())
+    }
+}
+
+pub trait TokenMethods: TokenMethodsClone {
     fn token_to_atp_line(&self) -> String;
     fn parse(&self, input: &str) -> String;
     fn get_string_repr(&self) -> String;
     fn token_from_vec_params(&mut self, line: Vec<String>) -> Result<(), String>;
+}
+
+impl Clone for Box<dyn TokenMethods> {
+    fn clone(&self) -> Box<dyn TokenMethods> {
+        self.clone_box()
+    }
 }
