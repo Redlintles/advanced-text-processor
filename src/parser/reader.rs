157963746d7450041595fa15a0bc77ddf8bb1e0d
redlintles <treelands54@gmail.com>
2025-05-30 15:55:17 -0300

    Adicionado tratamento de erros mais preciso à reader.rs

--- src/parser/reader.rs
+++ src/parser/reader.rs
@@ -4,36 +4,54 @@ use crate::data::{ TokenMethods };
 
 use super::get_supported_tokens;
 
-pub fn read_from_file(path: &str) -> Vec<Box<dyn TokenMethods>> {
+pub fn read_from_file(path: &str) -> Result<Vec<Box<dyn TokenMethods>>, String> {
     let mut result: Vec<Box<dyn TokenMethods>> = Vec::new();
 
-    let file = OpenOptions::new()
-        .read(true)
-        .open(path)
-        .expect("An ATP parsing error ocurred: File not found");
+    let file = match OpenOptions::new().read(true).open(path) {
+        Ok(x) => x,
+        Err(_) => {
+            return Err("An ATP parsing error ocurred: File not found".to_string());
+        }
+    };
 
     let reader = BufReader::new(file);
 
     for line in reader.lines() {
-        let mut line_text = line.expect("An atp Parsing error ocurred: Error reading file line");
+        let mut line_text = match line {
+            Ok(x) => x,
+            Err(_) => {
+                return Err("An ATP Parsing error ocurred: Error reading file line".to_string());
+            }
+        };
 
         line_text.pop();
 
-        let chunks = shell_words
-            ::split(&line_text)
-            .expect("An ATP parsing error ocurred: Shell Words internal error");
-
-        let parse_err_msg = "An ATP parsing error ocurred: Invalid Sintax";
+        let chunks = match shell_words::split(&line_text) {
+            Ok(x) => x,
+            Err(_) => {
+                return Err("An ATP parsing error ocurred: Shell Words internal error".to_string());
+            }
+        };
 
         let supported_tokens = get_supported_tokens();
-        let token_factory = supported_tokens.get(&chunks[0]).expect(parse_err_msg);
+        let token_factory = match supported_tokens.get(&chunks[0]) {
+            Some(x) => x,
+            None => {
+                return Err("An ATP parsing error ocurred: Invalid Sintax".to_string());
+            }
+        };
 
         let mut token = token_factory();
 
-        token.token_from_vec_params(chunks).expect("Parsing error, Invalid Token");
+        match token.token_from_vec_params(chunks) {
+            Ok(x) => x,
+            Err(_) => {
+                return Err("An Atp parsing error ocurred: Invalid token".to_string());
+            }
+        }
 
         result.push(token);
     }
 
-    result
+    Ok(result)
 }
