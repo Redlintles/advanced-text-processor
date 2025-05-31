180f99b427e3e074cd046da4324578a60b11e457
redlintles <treelands54@gmail.com>
2025-05-31 18:33:26 -0300

    Adicionado módulo bytecode parser, definido enum TokenOpCodes e trait BytecodeTokenMethods para permitir a implementação posterior do parser

--- src/bytecode_parser/mod.rs
+++ src/bytecode_parser/mod.rs
@@ -1,7 +1,9 @@
+use crate::data::TokenMethods;
+
 pub mod writer;
 
 #[repr(u8)]
-#[derive(Clone, Copy, Debug)]
+#[derive(Clone, Copy, Debug, PartialEq)]
 pub enum TokenOpCodes {
     AddToBeginning = 0x01,
     AddToEnd = 0x02,
@@ -19,20 +21,29 @@ pub enum TokenOpCodes {
     RotateLeft = 0x0e,
     RotateRight = 0x0f,
 }
-pub enum Operand {
-    Str(String),
-    Int(usize),
-}
 
 pub struct BytecodeInstruction {
     pub op_code: TokenOpCodes,
-    pub operands: Vec<Operand>,
+    pub operands: Vec<String>,
 }
 
-pub trait BytecodeTokenMethods {
+impl BytecodeInstruction {
+    pub fn to_bytecode_line(&self) -> String {
+        let mut result = format!("{:#04x}", self.op_code as u8);
+        for operand in self.operands.iter() {
+            result = format!("{} {}", result, operand);
+        }
+
+        result.push_str("\n");
+
+        result
+    }
+}
+
+pub trait BytecodeTokenMethods: TokenMethods {
     fn token_from_bytecode_instruction(
         &mut self,
         instruction: BytecodeInstruction
     ) -> Result<(), String>;
-    fn token_to_bytecode_instruction(&self) -> Result<String, String>;
+    fn token_to_bytecode_instruction(&self) -> BytecodeInstruction;
 }
