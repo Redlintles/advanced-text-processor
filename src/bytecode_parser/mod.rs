228085ddb40a25b110edd61fb3d989b5692be2ee
redlintles <treelands54@gmail.com>
2025-05-31 20:38:00 -0300

    Adicionada função get_supported_bytecode_tokens para mapear tokens para seus respectivos op_codes

--- src/bytecode_parser/mod.rs
+++ src/bytecode_parser/mod.rs
@@ -1,29 +1,31 @@
-use crate::data::TokenMethods;
+use std::collections::HashMap;
+
+use crate::data::{
+    token_defs::{
+        atb::Atb,
+        ate::Ate,
+        dla::Dla,
+        dlb::Dlb,
+        dlc::Dlc,
+        dlf::Dlf,
+        dll::Dll,
+        raw::Raw,
+        rfw::Rfw,
+        rpt::Rpt,
+        rtl::Rtl,
+        rtr::Rtr,
+        tbs::Tbs,
+        tls::Tls,
+        trs::Trs,
+    },
+    TokenMethods,
+};
 
 pub mod writer;
-
-#[repr(u8)]
-#[derive(Clone, Copy, Debug, PartialEq)]
-pub enum TokenOpCodes {
-    AddToBeginning = 0x01,
-    AddToEnd = 0x02,
-    DeleteFirst = 0x03,
-    DeleteLast = 0x04,
-    TrimBothSides = 0x05,
-    TrimLeftSide = 0x06,
-    TrimRightSide = 0x07,
-    DeleteChunk = 0x08,
-    DeleteAfter = 0x09,
-    DeleteBefore = 0x0a,
-    ReplaceAllWith = 0x0b,
-    ReplaceFirstWith = 0x0c,
-    Repeat = 0x0d,
-    RotateLeft = 0x0e,
-    RotateRight = 0x0f,
-}
+pub mod reader;
 
 pub struct BytecodeInstruction {
-    pub op_code: TokenOpCodes,
+    pub op_code: u8,
     pub operands: Vec<String>,
 }
 
@@ -46,4 +48,36 @@ pub trait BytecodeTokenMethods: TokenMethods {
         instruction: BytecodeInstruction
     ) -> Result<(), String>;
     fn token_to_bytecode_instruction(&self) -> BytecodeInstruction;
+
+    fn get_opcode(&self) -> u8;
+}
+
+pub fn token_from_hex_string(s: &str) -> Option<u8> {
+    let stripped = s.strip_prefix("0x")?;
+
+    let byte = u8::from_str_radix(stripped, 16).ok()?;
+
+    Some(byte)
+}
+
+pub fn get_supported_bytecode_tokens() -> HashMap<u8, fn() -> Box<dyn BytecodeTokenMethods>> {
+    let mut result: HashMap<u8, fn() -> Box<dyn BytecodeTokenMethods>> = HashMap::new();
+
+    result.insert(0x01, || Box::new(Atb::new()));
+    result.insert(0x02, || Box::new(Ate::new()));
+    result.insert(0x03, || Box::new(Dlf::new()));
+    result.insert(0x04, || Box::new(Dll::new()));
+    result.insert(0x05, || Box::new(Tbs::new()));
+    result.insert(0x06, || Box::new(Tls::new()));
+    result.insert(0x07, || Box::new(Trs::new()));
+    result.insert(0x08, || Box::new(Dlc::new()));
+    result.insert(0x09, || Box::new(Dla::new()));
+    result.insert(0x0a, || Box::new(Dlb::new()));
+    result.insert(0x0b, || Box::new(Raw::new()));
+    result.insert(0x0c, || Box::new(Rfw::new()));
+    result.insert(0x0d, || Box::new(Rpt::new()));
+    result.insert(0x0e, || Box::new(Rtl::new()));
+    result.insert(0x0f, || Box::new(Rtr::new()));
+
+    result
 }
