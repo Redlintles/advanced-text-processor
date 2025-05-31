69ca640fd2448ee27b966cd79bc00d96f1c24d7a
redlintles <treelands54@gmail.com>
2025-05-31 20:39:27 -0300

    Adicionada função get_opcode, exigida na trait BytecodeTokenMethods, em Dll

--- src/data/token_defs/dll.rs
+++ src/data/token_defs/dll.rs
@@ -1,7 +1,4 @@
-use crate::{
-    bytecode_parser::{ BytecodeInstruction, BytecodeTokenMethods, TokenOpCodes },
-    data::TokenMethods,
-};
+use crate::{ bytecode_parser::{ BytecodeInstruction, BytecodeTokenMethods }, data::TokenMethods };
 
 // Delete last
 #[derive(Clone, Copy)]
@@ -46,7 +43,7 @@ impl BytecodeTokenMethods for Dll {
         &mut self,
         instruction: BytecodeInstruction
     ) -> Result<(), String> {
-        if instruction.op_code == TokenOpCodes::DeleteLast {
+        if instruction.op_code == Dll::new().get_opcode() {
             return Ok(());
         }
 
@@ -55,8 +52,11 @@ impl BytecodeTokenMethods for Dll {
 
     fn token_to_bytecode_instruction(&self) -> BytecodeInstruction {
         BytecodeInstruction {
-            op_code: TokenOpCodes::DeleteLast,
+            op_code: Dll::new().get_opcode(),
             operands: [].to_vec(),
         }
+    }
+    fn get_opcode(&self) -> u8 {
+        0x04
     }
 }
