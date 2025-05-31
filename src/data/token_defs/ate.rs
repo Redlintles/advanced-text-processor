fd01a6e55998e9aeec44af778f1177b35fe54c3c
redlintles <treelands54@gmail.com>
2025-05-31 18:33:56 -0300

    Implementada trait BytecodeTokenMethods para Ate

--- src/data/token_defs/ate.rs
+++ src/data/token_defs/ate.rs
@@ -1,4 +1,7 @@
-use crate::data::{ TokenMethods };
+use crate::{
+    bytecode_parser::{ BytecodeInstruction, BytecodeTokenMethods, TokenOpCodes },
+    data::TokenMethods,
+};
 // add to end
 #[derive(Clone)]
 pub struct Ate {
@@ -38,5 +41,30 @@ impl TokenMethods for Ate {
 
     fn get_string_repr(&self) -> String {
         "ate".to_string()
+    }
+}
+
+impl BytecodeTokenMethods for Ate {
+    fn token_from_bytecode_instruction(
+        &mut self,
+        instruction: BytecodeInstruction
+    ) -> Result<(), String> {
+        if instruction.op_code == TokenOpCodes::AddToEnd {
+            if !instruction.operands[0].is_empty() {
+                self.text = instruction.operands[0].clone();
+                return Ok(());
+            }
+
+            return Err("An ATP Bytecode parsing error ocurred: Invalid Operands".to_string());
+        }
+
+        Err("An ATP Bytecode parsing error ocurred: Invalid Token".to_string())
+    }
+
+    fn token_to_bytecode_instruction(&self) -> BytecodeInstruction {
+        BytecodeInstruction {
+            op_code: TokenOpCodes::AddToEnd,
+            operands: [self.text.clone()].to_vec(),
+        }
     }
 }
