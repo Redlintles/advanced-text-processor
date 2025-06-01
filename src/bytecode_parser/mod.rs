52d2890faf452f9b0364541897e91493dbb38b12
redlintles <treelands54@gmail.com>
2025-06-01 18:41:25 -0300

    Adicionada nova função que mapeia Strings para BytecodeTokenMethods, alterado nome das funções de mapping, adicionada função token_to_bytecode_token_convert

--- src/bytecode_parser/mod.rs
+++ src/bytecode_parser/mod.rs
@@ -23,6 +23,7 @@ use crate::data::{
 
 pub mod writer;
 pub mod reader;
+pub mod transformer;
 
 pub struct BytecodeInstruction {
     pub op_code: u8,
@@ -60,24 +61,78 @@ pub fn token_from_hex_string(s: &str) -> Option<u8> {
     Some(byte)
 }
 
-pub fn get_supported_bytecode_tokens() -> HashMap<u8, fn() -> Box<dyn BytecodeTokenMethods>> {
-    let mut result: HashMap<u8, fn() -> Box<dyn BytecodeTokenMethods>> = HashMap::new();
+pub fn token_to_bytecode_token_convert(
+    token: Box<dyn TokenMethods>
+) -> Result<Box<dyn BytecodeTokenMethods>, String> {
+    let mut line = token.token_to_atp_line();
 
-    result.insert(0x01, || Box::new(Atb::new()));
-    result.insert(0x02, || Box::new(Ate::new()));
-    result.insert(0x03, || Box::new(Dlf::new()));
-    result.insert(0x04, || Box::new(Dll::new()));
-    result.insert(0x05, || Box::new(Tbs::new()));
-    result.insert(0x06, || Box::new(Tls::new()));
-    result.insert(0x07, || Box::new(Trs::new()));
-    result.insert(0x08, || Box::new(Dlc::new()));
-    result.insert(0x09, || Box::new(Dla::new()));
-    result.insert(0x0a, || Box::new(Dlb::new()));
-    result.insert(0x0b, || Box::new(Raw::new()));
-    result.insert(0x0c, || Box::new(Rfw::new()));
-    result.insert(0x0d, || Box::new(Rpt::new()));
-    result.insert(0x0e, || Box::new(Rtl::new()));
-    result.insert(0x0f, || Box::new(Rtr::new()));
+    line.pop();
 
-    result
+    let chunks = match shell_words::split(&line) {
+        Ok(x) => x,
+        Err(e) => {
+            return Err(e.to_string());
+        }
+    };
+
+    let string_token_map = get_map_str_to_bytecode_token();
+
+    let factory = match string_token_map.get(&token.get_string_repr()) {
+        Some(x) => x,
+        None => {
+            return Err("Invalid Token".to_string());
+        }
+    };
+
+    let mut new_token = factory();
+
+    new_token.token_from_vec_params(chunks)?;
+
+    Ok(new_token)
+}
+
+pub fn get_map_bytecode_to_bytecode_token() -> HashMap<u8, fn() -> Box<dyn BytecodeTokenMethods>> {
+    let mut bytecode_to_token: HashMap<u8, fn() -> Box<dyn BytecodeTokenMethods>> = HashMap::new();
+
+    bytecode_to_token.insert(0x01, || Box::new(Atb::new()));
+    bytecode_to_token.insert(0x02, || Box::new(Ate::new()));
+    bytecode_to_token.insert(0x03, || Box::new(Dlf::new()));
+    bytecode_to_token.insert(0x04, || Box::new(Dll::new()));
+    bytecode_to_token.insert(0x05, || Box::new(Tbs::new()));
+    bytecode_to_token.insert(0x06, || Box::new(Tls::new()));
+    bytecode_to_token.insert(0x07, || Box::new(Trs::new()));
+    bytecode_to_token.insert(0x08, || Box::new(Dlc::new()));
+    bytecode_to_token.insert(0x09, || Box::new(Dla::new()));
+    bytecode_to_token.insert(0x0a, || Box::new(Dlb::new()));
+    bytecode_to_token.insert(0x0b, || Box::new(Raw::new()));
+    bytecode_to_token.insert(0x0c, || Box::new(Rfw::new()));
+    bytecode_to_token.insert(0x0d, || Box::new(Rpt::new()));
+    bytecode_to_token.insert(0x0e, || Box::new(Rtl::new()));
+    bytecode_to_token.insert(0x0f, || Box::new(Rtr::new()));
+
+    bytecode_to_token
+}
+
+pub fn get_map_str_to_bytecode_token() -> HashMap<String, fn() -> Box<dyn BytecodeTokenMethods>> {
+    let mut string_to_token: HashMap<
+        String,
+        fn() -> Box<dyn BytecodeTokenMethods>
+    > = HashMap::new();
+    string_to_token.insert("atb".to_string(), || Box::new(Atb::new()));
+    string_to_token.insert("ate".to_string(), || Box::new(Ate::new()));
+    string_to_token.insert("dlf".to_string(), || Box::new(Dlf::new()));
+    string_to_token.insert("dll".to_string(), || Box::new(Dll::new()));
+    string_to_token.insert("tbs".to_string(), || Box::new(Tbs::new()));
+    string_to_token.insert("tls".to_string(), || Box::new(Tls::new()));
+    string_to_token.insert("trs".to_string(), || Box::new(Trs::new()));
+    string_to_token.insert("dlc".to_string(), || Box::new(Dlc::new()));
+    string_to_token.insert("dla".to_string(), || Box::new(Dla::new()));
+    string_to_token.insert("dlb".to_string(), || Box::new(Dlb::new()));
+    string_to_token.insert("raw".to_string(), || Box::new(Raw::new()));
+    string_to_token.insert("rfw".to_string(), || Box::new(Rfw::new()));
+    string_to_token.insert("rpt".to_string(), || Box::new(Rpt::new()));
+    string_to_token.insert("rtl".to_string(), || Box::new(Rtl::new()));
+    string_to_token.insert("rtr".to_string(), || Box::new(Rtr::new()));
+
+    string_to_token
 }
