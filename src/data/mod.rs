f6b32f0dffcf3cb6534bdd258528817df6043487
redlintles <treelands54@gmail.com>
2025-06-01 18:40:09 -0300

    Alterada ordem das declarações em data/mod.rs

--- src/data/mod.rs
+++ src/data/mod.rs
@@ -1,4 +1,12 @@
 pub mod token_defs;
+
+pub trait TokenMethods: TokenMethodsClone {
+    fn token_to_atp_line(&self) -> String;
+    fn parse(&self, input: &str) -> String;
+    fn get_string_repr(&self) -> String;
+    fn token_from_vec_params(&mut self, line: Vec<String>) -> Result<(), String>;
+}
+
 pub trait TokenMethodsClone {
     fn clone_box(&self) -> Box<dyn TokenMethods>;
 }
@@ -7,13 +15,6 @@ impl<T> TokenMethodsClone for T where T: 'static + TokenMethods + Clone {
     fn clone_box(&self) -> Box<dyn TokenMethods> {
         Box::new(self.clone())
     }
-}
-
-pub trait TokenMethods: TokenMethodsClone {
-    fn token_to_atp_line(&self) -> String;
-    fn parse(&self, input: &str) -> String;
-    fn get_string_repr(&self) -> String;
-    fn token_from_vec_params(&mut self, line: Vec<String>) -> Result<(), String>;
 }
 
 impl Clone for Box<dyn TokenMethods> {
