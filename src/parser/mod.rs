18987fcb10b2ef64ba922609c490426891131bd1
redlintles <treelands54@gmail.com>
2025-06-01 18:59:55 -0300

    Modificado get_supported_tokens para chamar ::default ao invés de ::new

--- src/parser/mod.rs
+++ src/parser/mod.rs
@@ -10,35 +10,35 @@ pub fn get_supported_tokens() -> HashMap<String, fn() -> Box<dyn TokenMethods>> 
     let mut tokens: HashMap<String, fn() -> Box<dyn TokenMethods>> = HashMap::new();
 
     // Add to Beginning
-    tokens.insert("atb".to_string(), || Box::new(atb::Atb::new()));
+    tokens.insert("atb".to_string(), || Box::new(atb::Atb::default()));
     // Add to end
-    tokens.insert("ate".to_string(), || Box::new(ate::Ate::new()));
+    tokens.insert("ate".to_string(), || Box::new(ate::Ate::default()));
     // Delete After
-    tokens.insert("dla".to_string(), || Box::new(dla::Dla::new()));
+    tokens.insert("dla".to_string(), || Box::new(dla::Dla::default()));
     // Delete before
-    tokens.insert("dlb".to_string(), || Box::new(dlb::Dlb::new()));
+    tokens.insert("dlb".to_string(), || Box::new(dlb::Dlb::default()));
     // Delete chunk
-    tokens.insert("dlc".to_string(), || Box::new(dlc::Dlc::new()));
+    tokens.insert("dlc".to_string(), || Box::new(dlc::Dlc::default()));
     // Delete first
-    tokens.insert("dlf".to_string(), || Box::new(dlf::Dlf::new()));
+    tokens.insert("dlf".to_string(), || Box::new(dlf::Dlf::default()));
     // Delete last
-    tokens.insert("dll".to_string(), || Box::new(dll::Dll::new()));
+    tokens.insert("dll".to_string(), || Box::new(dll::Dll::default()));
     // Replace all with
-    tokens.insert("raw".to_string(), || Box::new(raw::Raw::new()));
+    tokens.insert("raw".to_string(), || Box::new(raw::Raw::default()));
     // Replace first with
-    tokens.insert("rfw".to_string(), || Box::new(rfw::Rfw::new()));
+    tokens.insert("rfw".to_string(), || Box::new(rfw::Rfw::default()));
     // Repeat
-    tokens.insert("rpt".to_string(), || Box::new(rpt::Rpt::new()));
+    tokens.insert("rpt".to_string(), || Box::new(rpt::Rpt::default()));
     // Rotate right
-    tokens.insert("rtr".to_string(), || Box::new(rtr::Rtr::new()));
+    tokens.insert("rtr".to_string(), || Box::new(rtr::Rtr::default()));
     // Rotate left
-    tokens.insert("rtl".to_string(), || Box::new(rtl::Rtl::new()));
+    tokens.insert("rtl".to_string(), || Box::new(rtl::Rtl::default()));
     // Trim both sides
-    tokens.insert("tbs".to_string(), || Box::new(tbs::Tbs::new()));
+    tokens.insert("tbs".to_string(), || Box::new(tbs::Tbs::default()));
     // Trim left side
-    tokens.insert("tls".to_string(), || Box::new(tls::Tls::new()));
+    tokens.insert("tls".to_string(), || Box::new(tls::Tls::default()));
     // Trim right side
-    tokens.insert("trs".to_string(), || Box::new(trs::Trs::new()));
+    tokens.insert("trs".to_string(), || Box::new(trs::Trs::default()));
 
     tokens
 }
