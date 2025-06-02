f1bce41a9f8a8b00777f0b363148c3fbc41ecc7d
redlintles <treelands54@gmail.com>
2025-06-01 22:08:54 -0300

    Adicionado módulo Utils, para funções utilitárias internas

---
+++ src/utils/validations.rs
@@ -0,0 +1,32 @@
+use std::path::Path;
+
+pub fn check_file_path(path: &Path, ext: Option<&str>) -> Result<(), String> {
+    let parsed_ext = ext.unwrap_or("atp");
+    let mut v1: String = match path.extension() {
+        Some(x) => {
+            if x == parsed_ext {
+                "";
+            }
+            format!("Path does not match required extension {}", parsed_ext)
+        }
+        None => "Unable to get file extension".to_string(),
+    };
+
+    let v2: String = match path.parent() {
+        Some(x) => {
+            if x.exists() && path.is_dir() {
+                "";
+            }
+            "Parent should be an already existing directory".to_string()
+        }
+        None => "Parent does not exists".to_string(),
+    };
+
+    v1.push_str(&v2);
+
+    if v1.is_empty() {
+        return Ok(());
+    } else {
+        return Err(v1);
+    }
+}
