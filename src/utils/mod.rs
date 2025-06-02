215125a9ac1f9a11f6be1badde5100057f7f4f8f
redlintles <treelands54@gmail.com>
2025-06-01 22:36:23 -0300

    Adicionada módulo transform, para conversão de tipos, adicionada função string_to_usize

--- src/utils/mod.rs
+++ src/utils/mod.rs
@@ -1,1 +1,2 @@
 pub mod validations;
+pub mod transforms;
---
+++ src/utils/transforms.rs
@@ -0,0 +1,6 @@
+pub fn string_to_usize(chunk: String) -> Result<usize, String> {
+    match chunk.parse() {
+        Ok(v) => Ok(v),
+        Err(_) => { Err("Parsing from string to usize failed".to_string()) }
+    }
+}
