10833c8c78fe91ffbdc4b798d9c2f6f418e168b2
redlintles <treelands54@gmail.com>
2025-06-01 22:44:12 -0300

    Corrigido erro na definição do parâmetro em string_to_usize

--- src/utils/transforms.rs
+++ src/utils/transforms.rs
@@ -1,4 +1,4 @@
-pub fn string_to_usize(chunk: String) -> Result<usize, String> {
+pub fn string_to_usize(chunk: &String) -> Result<usize, String> {
     match chunk.parse() {
         Ok(v) => Ok(v),
         Err(_) => { Err("Parsing from string to usize failed".to_string()) }
